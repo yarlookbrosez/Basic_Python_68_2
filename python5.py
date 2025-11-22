eeb97598315f614a5735768a569e7ef6774ee166
walid <yarlookbrosez30@gmail.com>
2025-11-22 15:31:04 +0700

    Loop

---
+++ python5.py
@@ -0,0 +1,27 @@
+"""""
+#
+#part"Loop
+#section: while loop
+#
+"""""
+i = 1
+while i <= 5:
+    print("Hello,python",i)
+    if i == 3:
+        break   
+    i += 1
+
+"""""
+#
+#part: for loop
+#section: for loop
+#
+#
+"""""
+This_list = ["apple", "banana", "cherry"]
+for x in This_list:
+    print(x)    
+    if x == "banana":
+        break
+    
+#
