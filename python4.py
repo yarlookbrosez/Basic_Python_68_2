3913a91d23e63e9d4bf5c4d031ec7c77e76d27db
walid <yarlookbrosez30@gmail.com>
2025-11-22 14:47:30 +0700

    Condition

---
+++ python4.py
@@ -0,0 +1,23 @@
+""""""
+#
+#part: Conditionl statements
+#
+"""""
+
+"""""
+x = 10
+y = 20
+if x < y:
+    print("x is less than y")
+    print(1)
+elif x > y:
+    print("x is greater than y")
+    print(2)
+elif x == y:
+    print("x is equal to y")
+    print(3)
+elif x != y:
+    print("error")    
+    print(4)
+
+""""""
