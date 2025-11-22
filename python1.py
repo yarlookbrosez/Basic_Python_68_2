cd500658cb20b055aa3541e3907c8fe1900368b4
walid <yarlookbrosez30@gmail.com>
2025-11-22 13:26:52 +0700

    variable

---
+++ python1.py
@@ -0,0 +1,52 @@
+"""
+#
+#Part: python comment
+#
+"""
+#This is a sample Python file with comment
+
+"""
+v = s / t
+v = ความเร็ว (m/s)
+s = ระยะทาง (m)
+t = เวลา (s)
+"""
+
+print ("Hello, World!") #Print a greeting message
+
+"""
+#
+#Part: Python Variable
+#
+"""
+
+x = 5 #Integer Variable
+y = "5" #String Variable
+print(x,y)
+
+x = str(5)
+y = int(10)
+z = float(15)
+print(type(x),type(y),type(z))
+
+"""
+#
+#Part: Variable Naming
+#
+"""
+myvar = "John"
+my_var = "Bob"
+_my_var = "Alice"
+myVAR = "John"
+MYVAR =  "John"
+MY_VAR = "Bob"
+#Zoy_var = "John"
+#my-var = "John"
+#my var = "John"
+
+#Camel case
+myVariableName = "John"
+#Pascal Case
+MyVariableName = "John"
+#Snake case
+my_variable_name = "John"
