1a680dbd70cae27536d6fee5ddee251b0c990c18
walid <yarlookbrosez30@gmail.com>
2025-11-22 15:31:22 +0700

    function

---
+++ python6.py
@@ -0,0 +1,24 @@
+""""
+#
+#part: function
+#
+""""
+def myduction(firstname ="unknown", lastname ="simson")
+    print("My name is " + firstname + " " + lastname)
+    return fulname
+
+print(myfulname("hommer","simson"))
+print(myfulname("bath","simson"))
+print(myfulname("lisa","simson"))
+print(myfulname("marge","simsom"))
+print(myfulname("maggie","simson"))
+
+def xpPotion(xp):
+    xp += 100
+    return xp
+
+currentxp = 200
+print("current xp:", currentxp)
+currentxp = xpPotion(currentxp)
+print("after potion xp:", currentxp)
+  # """"""
