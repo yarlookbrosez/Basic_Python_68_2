759d39c868dd6cbfb6c26c8770adffa1de61e010
walid <yarlookbrosez30@gmail.com>
2025-11-22 14:33:08 +0700

    collections

---
+++ python3.py
@@ -0,0 +1,54 @@
+""""
+#
+#part: collections of python
+#list, tuple, set, dictionary
+#
+"""""
+
+
+""""""
+#
+#List
+#
+""""""
+This_list = ["apple", "banana", "cherry","apple","cherry"]
+print(This_list)
+print(This_list[1])
+print(type(This_list))
+print(len(This_list))
+
+
+""""""""
+#
+#tuple
+#
+this_tuple = ("apple", "banana", "cherry","apple","cherry")
+print(this_tuple)
+print(this_tuple[1])
+print(type(this_tuple))
+print(len(this_tuple))
+
+""""""
+#
+#set
+#
+""""""
+This_set = {"apple", "banana", "cherry","apple","cherry"}
+print(This_set)
+print(type(This_set))
+print(len(This_set))
+
+""""""
+#
+#dictionary
+#
+""""""
+this_dict = {
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+}
+print(this_dict)
+print(this_dict["name"])
+print(type(this_dict))
+print(len(this_dict))
